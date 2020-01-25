d9799c134d925389d330ac5aa5e0622224f6253b
rishij-at-803992426592 <rishi18304@iiitd.ac.in>
2020-01-25 12:35:39 +0530

    ~

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify, abort
+from hcd_menu import menuJson
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -9,7 +10,7 @@ def index():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', menu_value= menuJson), 404
 
 if(__name__ == "__main__"):
     app.run(host='0.0.0.0', port=8080)
---
+++ hcd_menu.py
@@ -0,0 +1,25 @@
+menuJson=[{
+        'categories': 'Academics >',
+        'number': '1'
+    },
+    {
+        'categories': 'Research >',
+        'number': '2'
+    },
+    {
+        'categories': 'People >',
+        'number': '3'
+    },
+    {
+        'categories': 'Events >',
+        'number': '4'
+    },
+    {
+        'categories': 'Facilities >',
+        'number': '5'
+    },
+    {
+        'categories': 'Courses >',
+        'number': '6'
+    }
+]
