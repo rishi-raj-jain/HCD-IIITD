fb7ecbe9b5c73cdbf3ed993e2caa1d6949aed0e4
rishij-at-803992426592 <rishi18304@iiitd.ac.in>
2020-01-27 21:14:25 +0530

    ~

--- hcd_menu.py
+++ hcd_menu.py
@@ -1,25 +1,73 @@
 menuJson=[{
-        'categories': 'Academics >',
-        'number': '1'
+        'categories': 'Academics',
+        'number': '1',
+        'sub_menu': [
+            {
+                'sub_category': 'Reset_1'
+            },
+            {
+                'sub_category': 'Reset_2'
+            },
+        ]
     },
     {
-        'categories': 'Research >',
-        'number': '2'
+        'categories': 'Research',
+        'number': '2',
+        'sub_menu': [
+            {
+                'sub_category': 'Reset_1'
+            },
+            {
+                'sub_category': 'Reset_2'
+            },
+        ]
     },
     {
-        'categories': 'People >',
-        'number': '3'
+        'categories': 'People',
+        'number': '3',
+        'sub_menu': [
+            {
+                'sub_category': 'Reset_1'
+            },
+            {
+                'sub_category': 'Reset_2'
+            },
+        ]
     },
     {
-        'categories': 'Events >',
-        'number': '4'
+        'categories': 'Events',
+        'number': '4',
+        'sub_menu': [
+            {
+                'sub_category': 'Reset_1'
+            },
+            {
+                'sub_category': 'Reset_2'
+            },
+        ]
     },
     {
-        'categories': 'Facilities >',
-        'number': '5'
+        'categories': 'Facilities',
+        'number': '5',
+        'sub_menu': [
+            {
+                'sub_category': 'Reset_1'
+            },
+            {
+                'sub_category': 'Reset_2'
+            },
+        ]
     },
     {
-        'categories': 'Courses >',
-        'number': '6'
+        'categories': 'Courses',
+        'number': '6',
+        'sub_menu': [
+            {
+                'sub_category': 'Reset_1'
+            },
+            {
+                'sub_category': 'Reset_2'
+            },
+        ]
     }
 ]
