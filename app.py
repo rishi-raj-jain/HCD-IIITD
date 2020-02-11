891df65fb8cbfe3c99e12d7415cc31667901f758
rishij-at-803992426592 <rishi18304@iiitd.ac.in>
2020-02-11 13:44:18 +0530

    Courses Page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import random
 from flask import Flask, render_template, request, jsonify, abort
 from hcd_menu import menuJson
+from course_list import courseJson
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -8,6 +9,10 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route("/")
 def index():
     return render_template('index.html', menu_value= menuJson)
+
+@app.route("/courses/")
+def courses():
+    return render_template('courses.html', menu_value= menuJson, courses_list= courseJson)
 
 @app.route("/contact/")
 def contact():
---
+++ course_list.py
@@ -0,0 +1,26 @@
+courseJson=[{
+        'Course_Name': 'Design Drawing and Visualization',
+        'Code': 'DES101',
+        'Course_Text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod'
+    },
+    {
+        'Course_Name': 'Design Drawing and Visualization',
+        'Code': 'DES101',
+        'Course_Text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod'
+    },
+    {
+        'Course_Name': 'Design Drawing and Visualization',
+        'Code': 'DES101',
+        'Course_Text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod'
+    },
+    {
+        'Course_Name': 'Design Drawing and Visualization',
+        'Code': 'DES101',
+        'Course_Text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod'
+    },
+    {
+        'Course_Name': 'Design Drawing and Visualization',
+        'Code': 'DES101',
+        'Course_Text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod'
+    }
+]
