936618cda3b7be9bfb980cab72527af50d5f0c02
rishij-at-803992426592 <rishi18304@iiitd.ac.in>
2020-02-12 01:39:25 +0530

    Content Updated

--- course_list.py
+++ course_list.py
@@ -1,26 +1,56 @@
 courseJson=[{
-        'Course_Name': 'Design Drawing and Visualization',
-        'Code': 'DES101',
-        'Course_Text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod'
+        'Course_Name': 'Introduction to Engineering Design',
+        'Code': 'DES130',
+        'Course_Text': 'Introduction to Engineering Design is a core, multidisciplinary course offered with an aim to ignite the young minds with concepts in design and innovation. Using the tools and skills learnt in the lab, the students participate in a project challenge to build functional prototypes in the field of intelligent buildings, automotive, and robotics which will provide solutions to real life problems.'
     },
     {
         'Course_Name': 'Design Drawing and Visualization',
         'Code': 'DES101',
-        'Course_Text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod'
+        'Course_Text': 'This course fosters understanding of drawing and sketching as a means to develop observational skills through the study of the environment and as a tool for visual representation, ideation/conceptualization, visualization and communication or presentation of design ideas through sketching and drawing from both observation and memory.'
     },
     {
-        'Course_Name': 'Design Drawing and Visualization',
-        'Code': 'DES101',
-        'Course_Text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod'
+        'Course_Name': 'Visual Design & Communication',
+        'Code': 'DES202',
+        'Course_Text': 'For a designer to communicate more concisely and in a visually appropriate manner, it is necessary to use commonly understood principles, perspective and design layout standards. Together, these conventions constitute a visual language, and help to ensure that the drawing is clear and relatively easy to understand.'
     },
     {
-        'Course_Name': 'Design Drawing and Visualization',
-        'Code': 'DES101',
-        'Course_Text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod'
+        'Course_Name': 'Design Processes and Perspectives',
+        'Code': 'DES201',
+        'Course_Text': 'Broadly, the course gives students the opportunity to develop essential design thinking skills such as exploring the designed space to identify problem, applying the design thinking process to problems, visualizing design solutions, refining final designs and communicating ideas in visually appropriate form through assignments and projects.'
     },
     {
-        'Course_Name': 'Design Drawing and Visualization',
-        'Code': 'DES101',
-        'Course_Text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod'
+        'Course_Name': 'Animation & Graphics',
+        'Code': 'DES302',
+        'Course_Text': 'This course will take you right through the fundamentals of Graphic Design from photorealism up to the point where fantasy and imagination begins. You will understand usage of the colour wheel and its role in creating Digital Art.'
+    },
+    {
+        'Course_Name': 'Film Making and Radio Podcasting',
+        'Code': 'DES303',
+        'Course_Text': 'This course will not only give you the basic technical skills but will also hand hold you into making a aesthetically correct decisions in assembling a film.'
+    },
+    {
+        'Course_Name': 'Wearable Applications, Research, Devices, Interactions (WARDI)',
+        'Code': 'DES513',
+        'Course_Text': 'This is a course about the current paradigm of Wearable Computing. In this course, we will cover the origins, pioneering contributions, and principles of Wearable Computing. With this foundation, we will initiate our exploration into the space by learning how to design physical (device form factor), digital (applications) as well as human (interaction techniques) aspects of Wearables.'
+    },
+    {
+        'Course_Name': 'Digital Audio - (Procedural Game Audio, Algorithmic Composition & Sound Synthesis)',
+        'Code': 'DES514',
+        'Course_Text': 'This hands-on project-based course will introduce students to the world of digital audio. Topics include real-time sound synthesis, machine listening, procedural game audio, algorithmic composition, digital instrument design and sound design. '
+    },
+    {
+        'Course_Name': 'Information systems in Public Health',
+        'Code': 'DES5XX',
+        'Course_Text': 'This course will give an understanding of public health information systems. It will include key concepts of public health, sources of public health information, ethics in public health practice and research, and an understanding of various public health information systems in use.'
+    },
+    {
+        'Course_Name': 'Game Development & Design',
+        'Code': 'DES512',
+        'Course_Text': 'This hands-on project-based course will introduce students to the fundamentals of game development & design using the Unreal 4 game engine. Topics include level design, lighting, materials, particle effects, game AI, game logic, user input mappings, audio, physics and motion.'
+    },
+    {
+        'Course_Name': ' Introduction to 3D Animation',
+        'Code': 'DES5XX',
+        'Course_Text': 'This course introduces students to: (i) Basics and fundamental principles of animation (ii) Workflow of animation (iii) Introduction to 3D Animation'
     }
 ]
